ee82ddeffc9433a6d7c68246a487cfcaa3c3a75b
Sohail-Fladdra <sohail.ansari@fladdra.com>
2023-09-09 20:18:36 +0530

    created student feature module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'student', loadChildren: () => import('./featured/student/student.module').then(m => m.StudentModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
