607c73328a4758520206def7e37f50a95ebb67ca
Sohail-Fladdra <sohail.ansari@fladdra.com>
2023-09-10 12:29:02 +0530

    made bsic components

--- src/app/featured/student/student.module.ts
+++ src/app/featured/student/student.module.ts
@@ -4,11 +4,15 @@ import { CommonModule } from '@angular/common';
 import { StudentRoutingModule } from './student-routing.module';
 import { StudentComponent } from './student.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { StudentFormComponent } from './pages/student-form/student-form.component';
+import { StudentListComponent } from './pages/student-list/student-list.component';
 
 
 @NgModule({
   declarations: [
-    StudentComponent
+    StudentComponent,
+    StudentFormComponent,
+    StudentListComponent
   ],
   imports: [
     CommonModule,
