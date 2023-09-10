85c1c7c9c2d946439adff9e2fb6a3336f6fe169f
Sohail-Fladdra <sohail.ansari@fladdra.com>
2023-09-10 12:07:06 +0530

    instaleld prime ng

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'student', loadChildren: () => import('./featured/student/student.module').then(m => m.StudentModule) }];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'student',
+    pathMatch: 'full',
+  },
+  {
+    path: 'student',
+    loadChildren: () =>
+      import('./featured/student/student.module').then((m) => m.StudentModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
